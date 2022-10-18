2e927ea0403cf9f77555faf2474f2fadec0e3c2f
Wilson del canto cepeda <wilsondelcanto.redes@gmail.com>
2022-10-18 01:17:53 -0300

    update

--- main.js
+++ main.js
@@ -1,19 +1,85 @@
-import { Usuario } from './class/Usuarios.js';
+const fs = require('fs');
+const path = require('path');
 
-const mascotas = ['Perro', 'Gato'];
+class Contenedor {
+  constructor(file) {
+    this.file = file;
+  }
 
-const user = new Usuario(
-  'Wilson',
-  'Del Canto',
-  [{ nombre: 'Fuego y Sangre', autor: 'George R.R. Martin.' }],
-  mascotas
-);
+  async save(obj) {
+    const data = await this.getAll();
+    obj.id = data.length + 1;
+    data.push(obj);
+    await fs.promises.writeFile(this.file, JSON.stringify(data, null, 2));
+    return obj.id;
+  }
 
-const main = () => {
-  console.log(user.getFullName());
-  user.addMascota('Raton');
-  console.log(user.countMascotas());
-  user.addBook('Juego de Tronos', 'George R.R. Martin.');
-  console.log(user.getBookNames());
-};
-main();
+  async getById(id) {
+    const data = await this.getAll();
+    return data.find(aProduct => aProduct.id === id);
+  }
+
+  async getAll() {
+    try {
+      const contenido = await fs.promises.readFile(this.file, 'utf-8');
+      return JSON.parse(contenido);
+    } catch (error) {
+      await fs.promises.writeFile(this.file, '[]', 'utf-8');
+      return [];
+    }
+  }
+
+  async deleteById(id) {
+    const data = await this.getAll();
+    const newData = data.filter(aProduct => aProduct.id !== id);
+    await fs.promises.writeFile(this.file, JSON.stringify(newData, null, 2));
+  }
+
+  async deleteAll() {
+    await fs.promises.writeFile(this.file, '[]', 'utf-8');
+  }
+
+  async getParsedFileOrCreateIfNotExists() {
+    try {
+      const contenido = await fs.promises.readFile(this.file, 'utf-8');
+      return JSON.parse(contenido);
+    } catch (error) {
+      await fs.promises.writeFile(this.file, '[]', 'utf-8');
+      return [];
+    }
+  }
+}
+
+(async () => {
+  const contenedor = new Contenedor(path.resolve(__dirname, 'productos.txt'));
+
+  await contenedor.save({
+    title: 'Escuadra',
+    price: 123.45,
+    thumbnail:
+      'https://cdn3.iconfinder.com/data/icons/education-209/64/ruler-triangle-stationary-school-256.png',
+  });
+  await contenedor.save({
+    title: 'Calculadora',
+    price: 234.56,
+    thumbnail:
+      'https://cdn3.iconfinder.com/data/icons/education-209/64/calculator-math-tool-school-256.png',
+  });
+  await contenedor.save({
+    title: 'Globo Terráqueo',
+    price: 345.67,
+    thumbnail:
+      'https://cdn3.iconfinder.com/data/icons/education-209/64/globe-earth-geograhy-planet-school-256.png',
+  });
+
+  console.log(await contenedor.getAll());
+  console.log(await contenedor.getById(2));
+
+  await contenedor.deleteById(1);
+
+  console.log(await contenedor.getAll());
+
+  await contenedor.deleteAll();
+
+  console.log(await contenedor.getAll());
+})();
