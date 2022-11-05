307f564f85b67f79a5fa20a4d4a358d0043093ad
Wilson Del Canto <wilsondelcanto.redes@gmail.com>
2022-11-05 11:20:37 -0300

    fix server y package

--- server.js
+++ server.js
@@ -7,13 +7,15 @@ app.get('/productos', async (solicitud, respuesta) => {
   const resp = await getAll();
   respuesta.send(resp);
 });
-server.on('Error', error => console.log(error));
 
 app.get('/productoRandom', async (solicitud, respuesta) => {
   const resp = await getRandom();
   respuesta.send(resp);
 });
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log('Server escuchando el puerto 8080');
 });
+server.on('Error', error => console.log(error));
+
+
