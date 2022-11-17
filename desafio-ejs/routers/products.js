550d6704f2dc4ea8e40348ca6d7e04c7c2ac21cf
Wilson Del Canto <wilsondelcanto.redes@gmail.com>
2022-11-17 11:39:04 -0300

    desafio 5

--- desafio-ejs/routers/products.js
+++ desafio-ejs/routers/products.js
@@ -1,5 +1,5 @@
 const { Router } = require('express');
-const ProductContenedor = require('../contenedores/ProductContenedor');
+const ProductContenedor = require('../container/Contenedor');
 
 const productRouter = Router();
 
---
+++ desafio-pug/middlewares/validErros.js
@@ -0,0 +1,13 @@
+const validErrors = (request, response, next) => {
+  if (!request.params.id) {
+    response
+      .json({
+        error: 'bad request, missing id',
+      })
+      .status(400);
+  } else {
+    next();
+  }
+};
+
+module.exports = validErrors;
